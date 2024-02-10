bac3365d9c088813ccbd019611c12a23b95e0e93
Fiido <roccobosio@gmail.com>
2024-02-10 18:46:22 -0300

    typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,27 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AppResolver } from './app.resolver';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    TypeOrmModule.forRoot({
+      host: process.env.DATABASE_HOST,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      type: 'postgres',
+      database: process.env.DATABASE_USER,
+      entities: [__dirname + '/entities/*.entity{.tx,.js}'],
+      synchronize: Boolean(process.env.DATABASE_SYNC),
+      dropSchema: true,
+      port: 5432,
     }),
     AppResolver,
   ],
